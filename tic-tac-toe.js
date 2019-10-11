e93e72488aa967fd7ead20950458377c1fe9d0f2
Jason Johnson <jvsonjohnson@gmail.com>
2019-10-11 15:35:58 -0500

    X class and innerHTML working

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,24 @@
 "use strict";
 
 window.onload = function() {
+  let turn = "X";
+  if (turn === "X") {
+    turn = "O";
+  } else {
+    turn = "X";
+  }
+
   var board = document.getElementById("board");
   var squares = board.getElementsByTagName("div");
+
   for (var i = 0; i < squares.length; i += 1) {
     squares[i].classList.add("square");
+    squares[i].addEventListener("click", function() {
+      this.classList.add(turn);
+      this.innerHTML = turn;
+    });
+
+    // this.innerHTML = "x";
   }
+  console.log(squares);
 };
