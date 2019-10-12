44e2321ddb90ed045e220d9e9a0e8bd4e60463d7
Jason Johnson <jvsonjohnson@gmail.com>
2019-10-11 23:22:07 -0500

    Currently working on disallowing cheating

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,7 +3,20 @@
 window.onload = function() {
   const board = document.getElementById("board");
   const squares = board.getElementsByTagName("div");
+  const status = document.getElementById("status");
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
   let playerTurn = "X";
+  var turn = [];
 
   for (var i = 0; i < squares.length; i += 1) {
     squares[i].classList.add("square");
@@ -13,6 +26,14 @@ window.onload = function() {
     squares[i].addEventListener("mouseout", function() {
       this.classList.remove("hover");
     });
+  }
+
+  function noCheats(square) {
+    if (square.classList.contains("X") || square.classList.contains("O")) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   function switchTurn() {
@@ -26,14 +47,18 @@ window.onload = function() {
   function XandO() {
     for (var i = 0; i < squares.length; i += 1) {
       squares[i].addEventListener("click", function() {
-        this.classList.add(playerTurn);
-        this.innerHTML = playerTurn;
-        switchTurn();
+        noCheats(squares[i]);
+        if (noCheats === false) {
+          this.classList.add(playerTurn);
+          this.innerHTML = playerTurn;
+          switchTurn();
+        }
       });
     }
   }
 
   XandO();
+
   // this.innerHTML = "x";
 
   console.log(squares);
