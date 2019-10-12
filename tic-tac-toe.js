06d25b6ef205ff74726eca0b81c477577844c904
Jason Johnson <jvsonjohnson@gmail.com>
2019-10-11 20:47:22 -0500

    Added an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,24 +1,33 @@
 "use strict";
 
 window.onload = function() {
-  let turn = "X";
-  if (turn === "X") {
-    turn = "O";
-  } else {
-    turn = "X";
-  }
-
   var board = document.getElementById("board");
   var squares = board.getElementsByTagName("div");
+  let playerTurn = "X";
 
   for (var i = 0; i < squares.length; i += 1) {
     squares[i].classList.add("square");
-    squares[i].addEventListener("click", function() {
-      this.classList.add(turn);
-      this.innerHTML = turn;
-    });
-
-    // this.innerHTML = "x";
   }
+  function switchTurn() {
+    if (playerTurn === "X") {
+      playerTurn = "O";
+    } else {
+      playerTurn = "X";
+    }
+  }
+
+  function XandO() {
+    for (var i = 0; i < squares.length; i += 1) {
+      squares[i].addEventListener("click", function() {
+        this.classList.add(playerTurn);
+        this.innerHTML = playerTurn;
+        switchTurn();
+      });
+    }
+  }
+
+  XandO();
+  // this.innerHTML = "x";
+
   console.log(squares);
 };
