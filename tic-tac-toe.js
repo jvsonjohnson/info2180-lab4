d874c7f43eca7d10c15a47a2175b3525dc24c3ea
Jason Johnson <jvsonjohnson@gmail.com>
2019-10-13 17:06:01 -0500

    App now checks for a winner and updates the status message appropriately

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -14,8 +14,8 @@ window.onload = function() {
     [0, 4, 8],
     [2, 4, 6]
   ];
+  var plays = new Array();
   let playerTurn = "X";
-  var gameOver = false;
 
   for (var i = 0; i < squares.length; i += 1) {
     squares[i].classList.add("square");
@@ -28,6 +28,72 @@ window.onload = function() {
     });
   }
 
+  function checkWinner(squares, playerTurn) {
+    if (
+      squares[0].innerHTML === playerTurn &&
+      squares[1].innerHTML === playerTurn &&
+      squares[2].innerHTML === playerTurn
+    ) {
+      return true;
+    }
+
+    if (
+      squares[3].innerHTML === playerTurn &&
+      squares[4].innerHTML === playerTurn &&
+      squares[5].innerHTML === playerTurn
+    ) {
+      return true;
+    }
+
+    if (
+      squares[6].innerHTML === playerTurn &&
+      squares[7].innerHTML === playerTurn &&
+      squares[8].innerHTML === playerTurn
+    ) {
+      return true;
+    }
+
+    if (
+      squares[0].innerHTML === playerTurn &&
+      squares[3].innerHTML === playerTurn &&
+      squares[6].innerHTML === playerTurn
+    ) {
+      return true;
+    }
+
+    if (
+      squares[1].innerHTML === playerTurn &&
+      squares[4].innerHTML === playerTurn &&
+      squares[7].innerHTML === playerTurn
+    ) {
+      return true;
+    }
+
+    if (
+      squares[2].innerHTML === playerTurn &&
+      squares[5].innerHTML === playerTurn &&
+      squares[8].innerHTML === playerTurn
+    ) {
+      return true;
+    }
+
+    if (
+      squares[0].innerHTML === playerTurn &&
+      squares[4].innerHTML === playerTurn &&
+      squares[8].innerHTML === playerTurn
+    ) {
+      return true;
+    }
+
+    if (
+      squares[2].innerHTML === playerTurn &&
+      squares[4].innerHTML === playerTurn &&
+      squares[6].innerHTML === playerTurn
+    ) {
+      return true;
+    }
+  }
+
   function switchTurn() {
     if (playerTurn === "X") {
       playerTurn = "O";
@@ -36,21 +102,25 @@ window.onload = function() {
     }
   }
 
-  function XandO() {
+  function playGame() {
     for (var i = 0; i < squares.length; i += 1) {
       squares[i].addEventListener("click", function() {
         if (this.innerHTML.length < 1) {
           this.classList.add(playerTurn);
           this.innerHTML = playerTurn;
+          if (checkWinner(squares, playerTurn)) {
+            status.innerHTML =
+              "Congratulations! " + playerTurn + " is the Winner!";
+            status.classList.add("you-won");
+          }
           switchTurn();
         }
       });
     }
   }
 
-  XandO();
-
-  // this.innerHTML = "x";
+  playGame();
 
   console.log(squares);
+  console.log(plays);
 };
