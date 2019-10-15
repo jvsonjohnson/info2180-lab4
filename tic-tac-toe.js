a4667fe381973c5ba7066c036e4321175ba0a5a5
Jason Johnson <jvsonjohnson@gmail.com>
2019-10-15 00:48:41 -0500

    New checkWinner Function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,7 +4,9 @@ window.onload = function() {
   var board = document.getElementById("board");
   var squares = board.getElementsByTagName("div");
   var status = document.getElementById("status");
-  var winningCombos = [
+  var button = document.getElementsByClassName("btn");
+  let playerTurn = "X";
+  const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -14,8 +16,6 @@ window.onload = function() {
     [0, 4, 8],
     [2, 4, 6]
   ];
-  var plays = new Array();
-  let playerTurn = "X";
 
   for (var i = 0; i < squares.length; i += 1) {
     squares[i].classList.add("square");
@@ -28,7 +28,28 @@ window.onload = function() {
     });
   }
 
-  function checkWinner(squares, playerTurn) {
+  this.positions = Array.from(document.querySelectorAll(".square"));
+  const positions = this.positions;
+
+  function checkWinner() {
+    var winner = false;
+    winningCombos.forEach(winningCombo => {
+      const pos0InnerText = positions[winningCombo[0]].innerHTML;
+      const pos1InnerText = positions[winningCombo[1]].innerHTML;
+      const pos2InnerText = positions[winningCombo[2]].innerHTML;
+      const isWinningCombo =
+        pos0InnerText != "" &&
+        pos0InnerText === pos1InnerText &&
+        pos1InnerText === pos2InnerText;
+
+      if (isWinningCombo) {
+        winner = true;
+      }
+    });
+    return winner;
+  }
+
+  /*function checkWinner(squares, playerTurn) {
     if (
       squares[0].innerHTML === playerTurn &&
       squares[1].innerHTML === playerTurn &&
@@ -92,7 +113,7 @@ window.onload = function() {
     ) {
       return true;
     }
-  }
+  }*/
 
   function switchTurn() {
     if (playerTurn === "X") {
@@ -102,13 +123,25 @@ window.onload = function() {
     }
   }
 
+  function resetGame() {
+    button.addEventListener("click", function() {
+      for (var i = 0; i < squares.length; i += 1) {
+        squares[i].removeClass(playerTurn);
+        squares[i].innerHTML = "";
+        status.innerHTML =
+          "Move your mouse over a square and click to play an X or an O.";
+        playerTurn = "X";
+      }
+    });
+  }
+
   function playGame() {
     for (var i = 0; i < squares.length; i += 1) {
       squares[i].addEventListener("click", function() {
         if (this.innerHTML.length < 1) {
           this.classList.add(playerTurn);
           this.innerHTML = playerTurn;
-          if (checkWinner(squares, playerTurn)) {
+          if (checkWinner()) {
             status.innerHTML =
               "Congratulations! " + playerTurn + " is the Winner!";
             status.classList.add("you-won");
@@ -120,7 +153,4 @@ window.onload = function() {
   }
 
   playGame();
-
-  console.log(squares);
-  console.log(plays);
 };
